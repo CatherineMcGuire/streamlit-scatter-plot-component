44bfdd16f11fd1f333307b8530feb2f65467fd90
Catherine McGuire <catherine.mcguire@manchester.ac.uk>
2022-07-11 16:46:51 +0100

    Fixes bug - zoom now persists after reload

--- my_component/__init__.py
+++ my_component/__init__.py
@@ -1,10 +1,53 @@
 import streamlit.components.v1 as components
 import streamlit as st
+from json import loads
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
 
 _my_component = components.declare_component(
     "my_component",
     url="http://localhost:3001/"
 )
 
-return_value = _my_component(name = "Streamlit", greeting = "Hello")
-st.write(return_value)
+def scatter_events(
+    plot_fig,
+    click_event=True,
+    select_event=True,
+    hover_event=False,
+    relay_event=True,
+    override_height=450,
+    override_width="100%",
+    key=None,
+    ):
+
+    return_value = _my_component(
+        plot_obj=plot_fig.to_json(),
+        override_height=override_height,
+        override_width=override_width,
+        key=key,
+        click_event=click_event,
+        select_event=select_event,
+        hover_event=hover_event,
+        relay_event=relay_event,
+        default="[]",  # Default return empty JSON list
+    )
+
+    return loads(return_value)
+
+st.set_page_config(layout="wide")
+
+st.subheader("Plotly Scatter Chart")
+
+    
+
+trace= go.Scatter(
+    x = [0, 1, 2, 3], 
+    y = [0, 1, 2, 3],
+    mode = 'markers'
+    )
+fig = go.Figure([trace])
+fig.layout.uirevision = True 
+plot_name_holder = st.empty()
+clickedPoint = scatter_events(fig, key="line")
+plot_name_holder.write(f"Clicked Point: {clickedPoint}")
--- my_component/frontend/src/MyComponent.tsx
+++ my_component/frontend/src/MyComponent.tsx
@@ -6,19 +6,24 @@ import {
 import React, { ReactNode } from "react"
 import Plot from 'react-plotly.js';
 
-// interface State {
-//   numClicks: number
-//   isFocused: boolean
-// }
-
-/**
- * This is a React-based component template. The `render()` function is called
- * automatically when your component should be re-rendered.
- */
 class MyComponent extends StreamlitComponentBase<State> {
-  public state = { numClicks: 0, isFocused: false, data: [], layout: {}, frames: [], config: {} }
+  public state = {data: [], layout: {}, frames: [], config: {}, style: {}, className: "" }
 
   public render = (): ReactNode => {
+
+    // Pull Plotly object from args and parse
+    const plot_obj = JSON.parse(this.props.args["plot_obj"]);
+    const override_height = this.props.args["override_height"];
+    const override_width = this.props.args["override_width"];
+
+    // Event booleans
+    const click_event = this.props.args["click_event"];
+    const select_event = this.props.args["select_event"];
+    const hover_event = this.props.args["hover_event"];
+    const relay_event = this.props.args["relay_event"];
+
+    Streamlit.setFrameHeight(override_height);
+
     // Arguments that are passed to the plugin in Python are accessible
     // via `this.props.args`. Here, we access the "name" arg.
     // const name = this.props.args["name"]
@@ -27,20 +32,20 @@ class MyComponent extends StreamlitComponentBase<State> {
     // Streamlit sends us a theme object via props that we can use to ensure
     // that our component has visuals that match the active theme in a
     // streamlit app.
-    const { theme } = this.props
-    const style: React.CSSProperties = {}
+    // const { theme } = this.props
+    // const style: React.CSSProperties = {}
 
-    // Maintain compatibility with older versions of Streamlit that don't send
-    // a theme object.
-    if (theme) {
-      // Use the theme object to style our button border. Alternatively, the
-      // theme style is defined in CSS vars.
-      const borderStyling = `1px solid ${
-        this.state.isFocused ? theme.primaryColor : "gray"
-      }`
-      style.border = borderStyling
-      style.outline = borderStyling
-    }
+    // // Maintain compatibility with older versions of Streamlit that don't send
+    // // a theme object.
+    // if (theme) {
+    //   // Use the theme object to style our button border. Alternatively, the
+    //   // theme style is defined in CSS vars.
+    //   const borderStyling = `1px solid ${
+    //     this.state.isFocused ? theme.primaryColor : "gray"
+    //   }`
+    //   style.border = borderStyling
+    //   style.outline = borderStyling
+    // }
 
     // Show a button and some text.
     // When the button is clicked, we'll increment our "numClicks" state
@@ -58,108 +63,138 @@ class MyComponent extends StreamlitComponentBase<State> {
       //   >
       //     Click Me!
       //   </button>
-      <span>
-        <Plot
-        onClick={this.onClicked}
-        onSelected={this.onSelected}
-        onRelayout={this.onRelayout}
-       data={[
-         {
-           x: [1, 2, 3],
-           y: [2, 6, 3],
-           type: 'scatter',
-           mode: 'markers',
-           marker: {color: 'red'},
-         },
-         {
-          x: [4, 3, 1],
-          y: [3, 6, 2],
-          type: 'scatter',
-          mode: 'markers',
-          marker: {color: 'blue'},
-        },
-       ]}
-       layout={ {title: 'Scatter Plot'} }
+      // <span>
+    //     <Plot
+    //     onClick={this.onClicked}
+    //     // onSelected={this.onSelected}
+    //    data={[
+    //      {
+    //        x: [1, 2, 3],
+    //        y: [2, 6, 3],
+    //        type: 'scatter',
+    //        mode: 'markers',
+    //        marker: {color: 'red'},
+    //      },
+    //      {
+    //       x: [4, 3, 1],
+    //       y: [3, 6, 2],
+    //       type: 'scatter',
+    //       mode: 'markers',
+    //       marker: {color: 'blue'},
+    //     },
+    //    ]}
+    //    layout={ {title: 'Scatter Plot'} }
+    //     onRelayout={this.onRelayout}
        
-     />
-      </span>
+    //  />
+    <Plot
+        data={plot_obj.data}
+        layout={plot_obj.layout}
+        config={plot_obj.config}
+        frames={plot_obj.frames}
+        onClick={click_event ? this.plotlyEventHandler : function(){}}
+        onSelected={select_event ? this.plotlyEventHandler : function(){}}
+        onRelayout={relay_event ? this.onRelayout : function(){}}
+        onHover={hover_event ? this.plotlyEventHandler : function(){}}
+        style={{width: override_width, height: override_height}}
+        className="stPlotlyChart"
+      />
+      // </span>
 
 
     )
   }
 
-  /** Click handler for our "Click Me!" button. */
-  private onClicked = (data: { points: any[]; }): any => {
-  // private onClicked = (): void => {
-    // Increment state.numClicks, and pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
-  //   this.setState(
-  //     prevState => ({ numClicks: prevState.numClicks + 1 }),
-  //     () => Streamlit.setComponentValue(this.state.numClicks)
-  //   )
-  // }
-  var clickedPoints: Array<any> = [];
-  data.points.forEach(function (arrayItem: any) {
-    clickedPoints.push({
-      x: arrayItem.x,
-      y: arrayItem.y,
-      curveNumber: arrayItem.curveNumber,
-      pointNumber: arrayItem.pointNumber,
-      pointIndex: arrayItem.pointIndex
-    })
-  });
+  private plotlyEventHandler = (data: any) => {
+    // Build array of points to return
+    var clickedPoints: Array<any> = [];
+    data.points.forEach(function (arrayItem: any) {
+      clickedPoints.push({
+        x: arrayItem.x,
+        y: arrayItem.y,
+        curveNumber: arrayItem.curveNumber,
+        pointNumber: arrayItem.pointNumber,
+        pointIndex: arrayItem.pointIndex
+      })
+    });
 
-  // Return array as JSON to Streamlit
-  Streamlit.setComponentValue(JSON.stringify(clickedPoints))
+    // Return array as JSON to Streamlit
+    Streamlit.setComponentValue(JSON.stringify(clickedPoints))
+  }
+  onRelayout = (data): any => {
+    // private onClicked = (): void => {
+      // Increment state.numClicks, and pass the new value back to
+      // Streamlit via `Streamlit.setComponentValue`.
+    //   this.setState(
+    //     prevState => ({ numClicks: prevState.numClicks + 1 }),
+    //     () => Streamlit.setComponentValue(this.state.numClicks)
+    //   )
+    // }
+    var relayoutPoints: Array<any> = [];
+
+      relayoutPoints.push({
+        event_type: 'relayout',
+        xaxis0: data['xaxis.range[0]'],
+        xaxis1: data['xaxis.range[1]'],
+        yaxis0: data['yaxis.range[0]'],
+        yaxis1: data['yaxis.range[1]'],
+      });
+
+    // Return array as JSON to Streamlit
+    Streamlit.setComponentValue(JSON.stringify(relayoutPoints))
+  }
 }
 
-private onSelected = (data: { points: any[]; }): any => {
-  // private onClicked = (): void => {
-    // Increment state.numClicks, and pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
-  //   this.setState(
-  //     prevState => ({ numClicks: prevState.numClicks + 1 }),
-  //     () => Streamlit.setComponentValue(this.state.numClicks)
-  //   )
-  // }
-  var selectedPoints: Array<any> = [];
-  data.points.forEach(function (arrayItem: any) {
-    selectedPoints.push({
-      x: arrayItem.x,
-      y: arrayItem.y,
-      curveNumber: arrayItem.curveNumber,
-      pointNumber: arrayItem.pointNumber,
-      pointIndex: arrayItem.pointIndex
-    })
-  });
+//   /** Click handler for our "Click Me!" button. */
+//   private onClicked = (data: { points: any[]; }): any => {
+//   // private onClicked = (): void => {
+//     // Increment state.numClicks, and pass the new value back to
+//     // Streamlit via `Streamlit.setComponentValue`.
+//   //   this.setState(
+//   //     prevState => ({ numClicks: prevState.numClicks + 1 }),
+//   //     () => Streamlit.setComponentValue(this.state.numClicks)
+//   //   )
+//   // }
+//   var clickedPoints: Array<any> = [];
+//   data.points.forEach(function (arrayItem: any) {
+//     clickedPoints.push({
+//       event_type: 'click',
+//       x: arrayItem.x,
+//       y: arrayItem.y,
+//       curveNumber: arrayItem.curveNumber,
+//       pointNumber: arrayItem.pointNumber,
+//       pointIndex: arrayItem.pointIndex
+//     })
+//   });
 
-  // Return array as JSON to Streamlit
-  Streamlit.setComponentValue(JSON.stringify(selectedPoints))
-}
+//   // Return array as JSON to Streamlit
+//   Streamlit.setComponentValue(JSON.stringify(clickedPoints))
+// }
 
-private onRelayout = (data: { points: any[]; }): any => {
-  // private onClicked = (): void => {
-    // Increment state.numClicks, and pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
-  //   this.setState(
-  //     prevState => ({ numClicks: prevState.numClicks + 1 }),
-  //     () => Streamlit.setComponentValue(this.state.numClicks)
-  //   )
-  // }
-  // var relayoutPoints: Array<any> = [];
-  // data.points.forEach(function (arrayItem: any) {
-  //   relayoutPoints.push({
-  //     x: arrayItem.x,
-  //     y: arrayItem.y,
-  //     curveNumber: arrayItem.curveNumber,
-  //     pointNumber: arrayItem.pointNumber,
-  //     pointIndex: arrayItem.pointIndex
-  //   })
-  // });
+// private onSelected = (data: { points: any[]; }): any => {
+//   // private onClicked = (): void => {
+//     // Increment state.numClicks, and pass the new value back to
+//     // Streamlit via `Streamlit.setComponentValue`.
+//   //   this.setState(
+//   //     prevState => ({ numClicks: prevState.numClicks + 1 }),
+//   //     () => Streamlit.setComponentValue(this.state.numClicks)
+//   //   )
+//   // }
+//   var selectedPoints: Array<any> = [];
+//   data.points.forEach(function (arrayItem: any) {
+//     selectedPoints.push({
+//       x: arrayItem.x,
+//       y: arrayItem.y,
+//       curveNumber: arrayItem.curveNumber,
+//       pointNumber: arrayItem.pointNumber,
+//       pointIndex: arrayItem.pointIndex
+//     })
+//   });
 
-  // Return array as JSON to Streamlit
-  Streamlit.setComponentValue(JSON.stringify(data))
-}
+//   // Return array as JSON to Streamlit
+//   Streamlit.setComponentValue(JSON.stringify(selectedPoints))
+// }
+
 
 
   /** Focus handler for our "Click Me!" button. */
@@ -171,7 +206,7 @@ private onRelayout = (data: { points: any[]; }): any => {
 //   private _onBlur = (): void => {
 //     this.setState({ isFocused: false })
 //   }
-}
+// }
 
 // "withStreamlitConnection" is a wrapper function. It bootstraps the
 // connection between your component and the Streamlit app, and handles
