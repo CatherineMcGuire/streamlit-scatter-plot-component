cfd4233c3b2794ba65eb787585f43b8054def83b
Catherine McGuire <catherine.mcguire@manchester.ac.uk>
2022-07-12 16:22:24 +0100

    Replaces init table data with default table data

--- my_component/__init__.py
+++ my_component/__init__.py
@@ -43,9 +43,12 @@ st.subheader("Plotly Scatter Chart")
 
 # DEFINE PLOT  
 
+x = [0, 6, 2, 9]
+y = [0, 1, 8, 3]
+
 trace= go.Scatter(
-    x = [0, 6, 2, 9], 
-    y = [0, 1, 8, 3],
+    x = x, 
+    y = y,
     mode = 'markers'
     )
 fig = go.Figure([trace])
@@ -60,28 +63,30 @@ Points = scatter_events(fig, key="line")
 
 # TABLE
 
-
+table_data = {'x': x, 'y': y}
+table_df =  pd.DataFrame(data=table_data)
+st.write(table_df);
 
 # Default - present all data points
 
-if Points[0]['event_type'] == 'init':
-    data = Points
-    x_values = Points[0]['x_values']
-    y_values = Points[0]['y_values']
-    init_df = pd.DataFrame(data=Points[0])
-    table_df = init_df[["x_values", "y_values"]].rename(columns={"x_values" : "x", "y_values" : "y"})
-    st.write(table_df);
+# if Points[0]['event_type'] == 'init':
+#     data = Points
+#     x_values = Points[0]['x_values']
+#     y_values = Points[0]['y_values']
+#     init_df = pd.DataFrame(data=Points[0])
+#     table_df = init_df[["x_values", "y_values"]].rename(columns={"x_values" : "x", "y_values" : "y"})
+#     st.write(table_df);
 
 # Zoom to select data set
 
-elif Points[0]['event_type'] == 'relayout':
-    st.write('RELAYOUT');
+# elif Points[0]['event_type'] == 'relayout':
+#     st.write(Points);
 
-# Click to compare dataset]
+# # Click to compare dataset]
 
-elif Points[0]['event_type'] == 'select':
-    st.write('SELECT');
+# elif Points[0]['event_type'] == 'select':
+#     st.write('SELECT');
 
-else:
-    st.write('NONE');
+# else:
+#     st.write('NONE');
 
